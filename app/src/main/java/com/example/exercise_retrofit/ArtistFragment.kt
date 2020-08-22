bc42859fd19ddeabae4f52074666e8905e8bf859
Muhammad Yusril Yuriis <61834564+myyuriis@users.noreply.github.com>
2020-08-22 16:38:48 +0700

    add post method
    not yet work

---
+++ app/src/main/java/com/example/exercise_retrofit/ArtistFragment.kt
@@ -0,0 +1,48 @@
+package com.example.exercise_retrofit
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.example.exercise_retrofit.artist.ArtistViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_artist.*
+
+class ArtistFragment : Fragment(), View.OnClickListener {
+
+    val artistViewModel by viewModels<ArtistViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_artist, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        artistViewModel.artist.observe(viewLifecycleOwner, Observer {
+            artistNameText.text = it.name
+        })
+        fetchButton.setOnClickListener(this)
+    }
+
+    fun getArtist() {
+        artistViewModel.getArtist(artistIdInputText.text.toString())
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0) {
+            fetchButton -> getArtist()
+        }
+    }
+}
--- app/src/main/java/com/example/exercise_retrofit/MainActivity.kt
+++ app/src/main/java/com/example/exercise_retrofit/MainActivity.kt
@@ -5,23 +5,58 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.exercise_retrofit.artist.ArtistViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
+//class MainActivity : AppCompatActivity() {
+////    val artistViewModel by viewModels<ArtistViewModel>()
+//    lateinit var navController: NavController
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+////        artistViewModel.artist.observe(this, Observer{
+////            artistNameText.text = it.name
+////        })
+//
+//    }
+////    fun getArtist(view: View){
+////        artistViewModel.getArtist(artistInputText.text.toString())
+////    }
+//}
+
 class MainActivity : AppCompatActivity() {
 
-    val artistViewModel by viewModels<ArtistViewModel>()
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        artistViewModel.artist.observe(this, Observer {
-            artistNameText.text = it.name
-        })
-    }
+        navController = (nav_host_fragment_container as NavHostFragment).navController
 
-    fun getArtist(view: View) {
-        artistViewModel.getArtist(artistInputText.text.toString())
+        NavigationUI.setupWithNavController(bottom_navigation, navController)
+
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.showArtist ->{
+                    println("SHOW ARTIST")
+                    navController.navigate(R.id.action_to_artistFragment)
+                    true
+                }
+                R.id.createArtist ->{
+                    println("CREATE ARTIST")
+                    navController.navigate(R.id.action_to_createArtisFragment)
+                    true
+                }
+                else -> {
+                    println("MASUK ELSE")
+                    true
+                }
+            }
+        }
     }
 }
